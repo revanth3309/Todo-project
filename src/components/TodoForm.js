3215e67f84f110aed0f5a54b0496b864b7e65ba8
revanth3309 <revanth3309@gmail.com>
2024-08-03 01:57:31 +0530

    check1

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 export const TodoForm=()=>{
+    const [value,setValue]=useState("")
+    const handleSubmit=e=>{
+        e.preventDefault()
+    }
     return (
-        <div>TodoForm</div>
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type='text' className='todo-input' placeholder='What is the task today?' onChange={(e)=>setValue(e.target.value)}/>
+            <button type='submit' className='todo-btn'>Add Task</button>
+        </form>
     )
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { TodoForm } from './TodoForm'
 export const TodoWrapper=()=>{
+    const [todos,setTodos]=useState([])
     return (
-        <div>TodoWrapper</div>
+        <div className='TodoWrapper'>
+            <TodoForm/>
+        </div>
+        
     )
 }
