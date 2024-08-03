b9178153050f093ee702cb2c87fbd83087ddef51
revanth3309 <revanth3309@gmail.com>
2024-08-03 14:32:27 +0530

    added functionality to delete icon

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,17 @@
 import React from 'react'
-export const Todo=()=>{
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+export const Todo=({task,toggleComplete,editTodo,deleteTodo})=>{
     return (
-        <div>Todo</div>
+        <div className='Todo'>
+            <p onClick={()=> toggleComplete(task.id)} className={`${task.completed?'completed':" "}`}>{task.task}</p>
+            <div>
+                <FontAwesomeIcon icon={faPenToSquare}
+                onClick={()=>editTodo(task.id)}/>
+                <FontAwesomeIcon icon={ faTrash}
+                onClick={()=>deleteTodo(task.id)}/>
+            </div>
+        </div>
     )
 }
